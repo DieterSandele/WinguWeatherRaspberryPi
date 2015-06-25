0d60af3a1e42c4b18c5de889ba0a5b4a8da403cd
Pawel <pspychalski@gmail.com>
2015-06-25 09:44:28 +0200

    perm change

--- lcd.py
+++ lcd.py
@@ -4,5 +4,5 @@ from time import *
 lcd = lcddriver.lcd()
 
 # lcd.lcd_clear();
-lcd.lcd_display_string("A Mama sprzata", 1)
-lcd.lcd_display_string("Ha ha ha", 2)
+lcd.lcd_display_string("Lorem", 1)
+lcd.lcd_display_string("Ipsum", 2)
